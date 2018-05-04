110642f8ebf73fd7f3a4cf3c0a9e360e7e7c257c
mail <aschuma@users.noreply.github.com>
2018-05-04 22:08:51 +0200

    #23 support for multiple wlan env - refactoring

--- boot.py
+++ boot.py
@@ -1,35 +1,57 @@
+def network_scan():
+    import network
+    
+    sta_if = network.WLAN(network.STA_IF)
+    sta_if.active(True)
+    available_nw_names = [str(nw[0], 'UTF-8') for nw in sta_if.scan()]
+    print('network::scan:', available_nw_names)
+    return available_nw_names
+
+
+def network_configurations():
+    import config
+    
+    available_nw_names = network_scan()
+    available_nw_config_list = [conf for conf in config.network for name in available_nw_names if conf['name'] == name]
+    print('network::known:', [config['name'] for config in available_nw_config_list])
+    return available_nw_config_list
+
+
 def network_connect():
     import network
-    import config
     from utime import sleep
-
+    
     sta_if = network.WLAN(network.STA_IF)
-    while not sta_if.isconnected():
-        for network in config.network:
-            if not sta_if.isconnected():
-                print('connecting to network ', network['name'], ' ...')
-                sta_if.active(True)
-                sta_if.connect(network['name'], network['pw'])
-                sleep(10)
-    print('network config:', sta_if.ifconfig())
+    if not sta_if.isconnected():
+        config_list = network_configurations()
+        config = config_list[0] if config_list else None
+        if config:
+            print('network::connecting to', config['name'])
+            sta_if.active(True)
+            sta_if.connect(config['name'], config['pw'])
+            while not sta_if.isconnected():
+                sleep(2)
+    
+    print('network::config:', sta_if.ifconfig())
 
 
 def network_disconnect():
     import network
+    
     network.WLAN(network.STA_IF).disconnect()
 
 
 def ntp_time_sync():
     import ntptime
     from utime import sleep, localtime
-
-    print('fetching UTC time ...')
+    
     time_set = False
     while not time_set:
         try:
+            print('ntp_sync::fetching')
             ntptime.settime()
             time_set = True
         except Exception as e:
-            print('could not set time:', str(e))
-            sleep(5)
-        print('utc time:', str(localtime()))
+            print('ntp_sync::except', str(e))
+            sleep(2)
+        print('ntp_sync::utc', str(localtime()))
